35d07f387b251bbe9343469b3fe6c6dfab244fbb
Shane McAndrew <55311029+shanemcandrewai@users.noreply.github.com>
2021-03-09 18:02:51 +0100

    put loop in testr

--- testr/testr/spiders/testr_spider.py
+++ testr/testr/spiders/testr_spider.py
@@ -4,17 +4,18 @@ from stem.control import Controller
 
 class TestrSpider(scrapy.Spider):
     name = "testr"
+    count = 0
 
     def start_requests(self):
-        with Controller.from_port(port = 9051) as c:
-            c.authenticate()
-            c.signal(Signal.NEWNYM)
         url = getattr(self, 'url', None)
         yield scrapy.Request(url=url, callback=self.parse)
-
+        
+        
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = f'quotes-{page}.html'
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log(f'Saved file {filename}')
+        count += 1
+        while count < 10:
+            with Controller.from_port(port = 9051) as c:
+                c.authenticate()
+                c.signal(Signal.NEWNYM)
+            yield scrapy.Request(response.url, callback=self.parse, dont_filter=True))
+            self.log(f'Saved file {count}')
