0d35876b7418f9699b84b72a4545311ba2fc2c52
Shane McAndrew <55311029+shanemcandrewai@users.noreply.github.com>
2021-03-10 17:03:44 +0100

    add delay to new id

--- testr/testr/spiders/testr_spider.py
+++ testr/testr/spiders/testr_spider.py
@@ -1,24 +1,28 @@
 import scrapy
 from stem import Signal
 from stem.control import Controller
+from time import sleep
 
 class TestrSpider(scrapy.Spider):
-    name = "testr"
+    name = 'testr'
     count = 0
 
     def start_requests(self):
         url = getattr(self, 'url', None)
-        yield scrapy.Request(url=url, callback=self.parse)
+        yield scrapy.Request(url=url, callback=self.parse, dont_filter=True)
+        yield scrapy.Request(url=url, callback=self.parse, dont_filter=True)
+        yield scrapy.Request(url=url, callback=self.parse, dont_filter=True)
+        yield scrapy.Request(url=url, callback=self.parse, dont_filter=True)
+        yield scrapy.Request(url=url, callback=self.parse, dont_filter=True)
         
     def parse(self, response):
-#        with Controller.from_port(port = 9051) as c:
-#            c.authenticate()
-#            c.signal(Signal.NEWNYM)
-        while TestrSpider.count < 10:
-            TestrSpider.count += 1
-            filename = f'{TestrSpider.count}.html'
-            with open(filename, 'wb') as f:
-                f.write(response.body)
-            self.log(f'Saved file {TestrSpider.count}')
-#            self.log('xxx', response.url)
-            yield scrapy.Request(response.url, callback=self.parse, dont_filter=True)
+        sleep(10)
+        self.log(f'Saved file {TestrSpider.count}')
+        with Controller.from_port(port = 9051) as c:
+            c.authenticate()
+            c.signal(Signal.NEWNYM)
+        TestrSpider.count += 1
+        filename = f'{TestrSpider.count}.html'
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log(response.url)
