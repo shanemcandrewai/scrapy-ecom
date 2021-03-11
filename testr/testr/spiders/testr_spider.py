0168536980d983468652114874aaa02de7615798
Shane McAndrew <55311029+shanemcandrewai@users.noreply.github.com>
2021-03-11 22:03:19 +0100

    update testr spid

--- testr/testr/spiders/testr_spider.py
+++ testr/testr/spiders/testr_spider.py
@@ -1,21 +1,13 @@
 import scrapy
-from stem import Signal
-from stem.control import Controller
+import os
 
 class TestrSpider(scrapy.Spider):
     name = "testr"
-    count = 0
 
     def start_requests(self):
         url = getattr(self, 'url', None)
         yield scrapy.Request(url=url, callback=self.parse)
         
-        
     def parse(self, response):
-        count += 1
-        while count < 10:
-            with Controller.from_port(port = 9051) as c:
-                c.authenticate()
-                c.signal(Signal.NEWNYM)
-            yield scrapy.Request(response.url, callback=self.parse, dont_filter=True))
-            self.log(f'Saved file {count}')
+        print('xxxxx')
+        print(response.url.split('/')[-2] + '.html')
