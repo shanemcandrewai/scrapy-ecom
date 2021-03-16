716ee082785a28e6171cb989a6f70104bfcd4a39
Shane McAndrew <55311029+shanemcandrewai@users.noreply.github.com>
2021-03-16 09:41:18 +0100

    fix url user

--- testr/testr/spiders/testr_spider.py
+++ testr/testr/spiders/testr_spider.py
@@ -1,17 +1,25 @@
 import scrapy
 import os
 from urllib.parse import urlparse
+import json
 
 class TestrSpider(scrapy.Spider):
     name = "testr"
+    urlp = ''
 
     def start_requests(self):
         url = getattr(self, 'url', None)
-        urlp = urlparse(url)
-        url_json = urlp.scheme + '://' + urlp.netloc + '/lrp/api/search?attributesById[]=' + urlp.fragment.split('|')[0].split(':')[1] + '&attributesByKey[]=' + urlp.fragment.split('|')[1].replace(':', '%3A') + '&l1CategoryId=1'
+        self.urlp = urlparse(url)
+        url_json = self.urlp.scheme + '://' + self.urlp.netloc + '/lrp/api/search?attributesById[]=' + self.urlp.fragment.split('|')[0].split(':')[1] + '&attributesByKey[]=' + self.urlp.fragment.split('|')[1].replace(':', '%3A') + '&l1CategoryId=1'
         yield scrapy.Request(url=url_json, callback=self.parse)
         
     def parse(self, response):
         with open('ant.json', 'wb') as f:
             f.write(response.body)
         self.log('xxx json saved')
+        items = json.loads(response.body)
+        url_seller = (self.urlp.scheme + '://' + self.urlp.netloc + '/u/'
+                     + items['listings'][0]['sellerInformation']['sellerName'].replace(
+                         ' ', '-').lower()
+                     + '/' + str(items['listings'][0]['sellerInformation']['sellerId']))
+        self.log('xxx ' + url_seller)
