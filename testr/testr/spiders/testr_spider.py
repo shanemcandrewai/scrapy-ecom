5e4c377beed15dbc2675c4b63f4968826b4cec7c
Shane McAndrew <55311029+shanemcandrewai@users.noreply.github.com>
2021-03-27 11:27:11 +0100

    recursion wip

--- testr/testr/spiders/testr_spider.py
+++ testr/testr/spiders/testr_spider.py
@@ -13,38 +13,45 @@ class TestrSpider(scrapy.Spider):
     def parse(self, response):
         items = json.loads(response.xpath('//body').re_first(
                 '>\{.*?\}\<')[1:-1])
-        extract = ['date', 'categoryId', 'verticals', 'title', 'priceCents',
+        extract_fields = ['date', 'categoryId', 'verticals', 'title', 'priceCents',
                    'priceType', 'sellerName', 'sellerId', 'cityName',
                    'countryAbbreviation', 'itemId']
-        ge = {}
-        for e in self.find_key(extract, items):
-            ge[str(e[0])] = e[1]
-            if 'sellerId' in e[0]:
-                sellerId = str(e[1])
-            if 'sellerName' in e[0]:
-                sellerName = e[1].replace(' ', '-').lower()
-                sellerName = sellerName.replace('.', '-')
-                sellerName = sellerName.replace('--', '-')
-                sellerName = sellerName.replace("'", '')
-                if sellerName[-1] == '-':
-                    sellerName = sellerName[:-1]
-                url_seller = ('/u/' + sellerName + '/' + sellerId + '/')
-                yield response.follow(url=url_seller, callback=self.parse)
-                for page in range(2, 10):
-                    yield response.follow(url=f"{url_seller}p/{page}/",
-                                          callback=self.parse)
-        yield ge
+
+        if '/u/' not in response.url:
+            seller_items = {}
+            for cat_item in self.find_key(extract_fields, items):
+                seller_items[str(cat_item[0])] = cat_item[1]
+                if 'sellerId' in cat_item[0]:
+                    sellerId = str(cat_item[1])
+                if 'sellerName' in cat_item[0]:
+                    sellerName = cat_item[1].replace(' ', '-').lower()
+                    sellerName = sellerName.replace('.', '-')
+                    sellerName = sellerName.replace('--', '-')
+                    sellerName = sellerName.replace("'", '')
+                    if sellerName[-1] == '-':
+                        sellerName = sellerName[:-1]
+                    url_seller = ('/u/' + sellerName + '/' + sellerId + '/')
+
+                    if response.follow(url=url_seller, callback=self.parse):
+                        self.log(f"xxxxx {url_seller}")
+                        np = self.get_next_page_url(response.url)
+                        self.log(f"xxxxx {np}")
+                        yield scrapy.Request(url=np,
+                                             callback=self.parse)
+            yield seller_items
         if items:
-            page_ind = response.url.find('p/')
-            slash_ind = response.url.rfind('/')
-            if page_ind == -1:
-                page_num = 1
-            else:
-                page_num = response.url[page_ind+2:slash_ind]
-            next_num = int(page_num) + 1
-            next_page = f"/p/{str(next_num)}"
-            yield scrapy.Request(url=next_page,
+            yield scrapy.Request(url= self.get_next_page_url(response.url),
                                  callback=self.parse)
+
+    def get_next_page_url(self, url):
+        page_ind = url.find('p/') - 1
+        slash_ind = url.rfind('/')
+        if page_ind == -1:
+            page_num = 1
+        else:
+            page_num = url[page_ind+2:slash_ind]
+        next_num = int(page_num) + 1
+        return f"{url[:page_ind]}/p/{str(next_num)}/"
 
     def find_key(self, keys, targ):
         """ Search JSON string `targ` for `keys`, return path and value """
