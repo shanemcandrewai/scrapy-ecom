65b5630d8d913d5b789f5b05dc7622a38a5f1c5d
Shane McAndrew <55311029+shanemcandrewai@users.noreply.github.com>
2021-04-09 15:46:45 +0200

    import post scrape

--- testr/proc_scrap.py
+++ testr/proc_scrap.py
@@ -1,1 +1,26 @@
-import jsonwith open('mp.json') as f:    dfd = json.load(f)csvl = []rec = {}for d in dfd:    for l in d['listings']:        rec = l.copy()        rec.update(d)        for v in rec['verticals']:            rec[v] = True        del rec['verticals']        rec[rec['sellerId']] = True        del rec['sellerId']        del rec['id']        del rec['listings']        csvl.append(rec)import pandas as pddf = pd.DataFrame(csvl)
+with open('mp.json') as f:
+    dfd = json.load(f)
+csvl = []
+rec = {}
+for d in dfd:
+    for l in d['listings']:
+        rec = l.copy()
+        rec.update(d)
+        rec[rec['periodSinceRegistrationDate']] = True
+        rec[rec['priceType']] = True
+        rec[rec['sellerId']] = True
+        rec[rec['categoryId']] = True
+        for v in rec['verticals']:
+            rec[v] = True
+        del rec['id']
+        del rec['periodSinceRegistrationDate']
+        del rec['title']
+        del rec['priceType']
+        del rec['date']
+        del rec['sellerId']
+        del rec['sellerName']
+        del rec['categoryId']
+        del rec['verticals']
+        del rec['listings']
+        csvl.append(rec)
+
